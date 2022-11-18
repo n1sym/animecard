b34c2a6c47dbb25cc6d5bcaa99e34cd8e5e01445
n1sym <owlbook248@gmail.com>
2022-11-18 23:31:56 +0900

    read

---
+++ src/components/Table.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let tables
+</script>
+
+<table>
+  <tbody>
+    {#each tables as row}
+      <tr>
+        <td id="c1">{row.name}</td>
+        <td>
+          {row.title}
+        </td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
+
+<style>
+  #c1 {
+    width: 60px;
+  }
+</style>
