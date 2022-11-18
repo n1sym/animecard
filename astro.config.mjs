c29673320c8026bb387e0269006f53944e4872ce
n1sym <owlbook248@gmail.com>
2022-11-18 22:50:04 +0900

    ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  output: 'server',
+  adapter: cloudflare()
 });
